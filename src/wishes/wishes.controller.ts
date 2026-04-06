1383fd8b449f6c2d6853535bc28c9b931632984c
Inthetouch <nikita_eremeev@mail.ru>
2026-04-06 22:59:42 +0300

    rework wishes

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -6,37 +6,59 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('wishes')
 export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
   @Post()
-  create(@Body() createWishDto: CreateWishDto) {
-    return this.wishesService.create(createWishDto);
+  @UseGuards(JwtAuthGuard)
+  create(@Req() req, @Body() createWishDto: CreateWishDto) {
+    return this.wishesService.create(req.user, createWishDto);
   }
 
-  @Get()
-  findAll() {
-    return this.wishesService.findAll();
+  @Get('last')
+  findLast() {
+    return this.wishesService.findLast();
+  }
+
+  @Get('top')
+  findTop() {
+    return this.wishesService.findTop();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.wishesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
-    return this.wishesService.update(+id, updateWishDto);
+  @UseGuards(JwtAuthGuard)
+  update(
+    @Req() req,
+    @Param('id') id: string,
+    @Body() updateWishDto: UpdateWishDto,
+  ) {
+    return this.wishesService.updateOne(+id, req.user.id, updateWishDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishesService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  remove(@Req() req, @Param('id') id: string) {
+    return this.wishesService.removeOne(+id, req.user.id);
+  }
+
+  @Post(':id/copy')
+  @UseGuards(JwtAuthGuard)
+  copy(@Req() req, @Param('id') id: string) {
+    return this.wishesService.copy(+id, req.user.id);
   }
 }
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,26 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wish } from './entities/wish.entity';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class WishesService {
-  create(createWishDto: CreateWishDto) {
-    return 'This action adds a new wish';
+  constructor(
+    @InjectRepository(Wish)
+    private readonly wishRepository: Repository<Wish>,
+  ) {}
+
+  async create(owner: User, createWishDto: CreateWishDto) {
+    const wish = this.wishRepository.create({ ...createWishDto, owner });
+    return await this.wishRepository.save(wish);
   }
 
-  findAll() {
-    return `This action returns all wishes`;
+  async findLast(): Promise<Wish[]> {
+    return this.wishRepository.find({
+      order: {
+        createdAt: 'DESC',
+      },
+      take: 40,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wish`;
+  async findTop(): Promise<Wish[]> {
+    return this.wishRepository.find({
+      order: {
+        copied: 'DESC',
+      },
+      take: 20,
+    });
   }
 
-  update(id: number, updateWishDto: UpdateWishDto) {
-    return `This action updates a #${id} wish`;
+  async findOne(id: number): Promise<Wish> {
+    const wish = await this.wishRepository.findOne({
+      where: { id },
+      relations: ['owner', 'offers', 'offers.user'],
+    });
+
+    if (!wish) {
+      throw new NotFoundException('Подарок не найден');
+    }
+    return wish;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} wish`;
+  async updateOne(
+    wishId: number,
+    userId: number,
+    UpdateWishDto: UpdateWishDto,
+  ) {
+    const wish = await this.findOne(wishId);
+
+    if (wish.owner.id !== userId) {
+      throw new ForbiddenException('Вы не можете редактировать чужие подарки');
+    }
+
+    if (UpdateWishDto.price && UpdateWishDto.price > 0) {
+      throw new ForbiddenException(
+        'Нельзя изменить стоимость: на подарок уже скидываются',
+      );
+    }
+    await this.wishRepository.update(wishId, UpdateWishDto);
+    return this.wishRepository.update(wishId, UpdateWishDto);
+  }
+
+  async removeOne(wishId: number, userId: number) {
+    const wish = await this.findOne(wishId);
+    if (wish.owner.id !== userId) {
+      throw new ForbiddenException('Вы не можете удалять чужие подарки');
+    }
+    await this.wishRepository.delete(wishId);
+    return wish;
+  }
+
+  async copy(wishId: number, user: User) {
+    const wish = await this.findOne(wishId);
+    await this.wishRepository.update(wishId, { copied: wish.copied + 1 });
+    const wishCopy = this.wishRepository.create({
+      name: wish.name,
+      link: wish.link,
+      image: wish.image,
+      price: wish.price,
+      description: wish.description,
+      owner: user,
+      raised: 0,
+      copied: 0,
+    });
+
+    return await this.wishRepository.save(wishCopy);
   }
 }
